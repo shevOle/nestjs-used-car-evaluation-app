f1a05a79d07fbae78cf43b69ae50ff70e7c2addf
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-02-21 22:47:19 +0200

    refactor: clean up default files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { Report } from './reports/report.entity';
@@ -20,7 +18,5 @@ import { AuthModule } from './auth/auth.module';
     ReportsModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
