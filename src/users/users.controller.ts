6457c42930b461f7bf0bdd2b05f138f36608f55f
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-02-21 22:45:09 +0200

    feat: implement user controller endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Patch, Body, Delete, Query, Param, BadRequestException } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private userService: UsersService) {}
+
+    @Get()
+    findUsers(@Query('email') email: string) {
+        if (email) return this.userService.findByEmail(email);
+        return this.userService.findAll();
+    }
+
+    @Get('/:id')
+    findUserById(@Param('id') id: string) {
+        if (!parseInt(id) && id !== '0') throw new BadRequestException('Id must be a number');
+        return this.userService.findById(parseInt(id));
+    }
+
+    @Patch('/:id')
+    updateUser(
+        @Param('id') id: string,
+        @Body() body: UpdateUserDto,
+    ) {
+        if (!parseInt(id) && id !== '0') throw new BadRequestException('Id must be a number');
+        return this.userService.update(parseInt(id), body);
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        if (!parseInt(id) && id !== '0') throw new BadRequestException('Id must be a number');
+        return this.userService.remove(parseInt(id));
+    }
+}
