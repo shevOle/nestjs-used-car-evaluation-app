1898714d5a7147b0c10ed903a4802872260b88c8
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-02-21 22:44:46 +0200

    feat: implement service methods for user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, Catch } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(User) private userRepository: Repository<User>,
+    ) {}
+
+    async findById(id: number): Promise<User> {
+        const user = await this.userRepository.findOneBy({ id });
+        if (!user) throw new NotFoundException('User not found');
+        return user;
+    }
+
+    async findByEmail(email: string): Promise<User[]> {
+        const user = await this.userRepository.find({ where: { email } });
+        if (!user.length) throw new NotFoundException('User not found');
+        return user;
+    }
+
+    findAll(): Promise<User[]> {
+        return this.userRepository.find({});
+    }
+
+    async update(id: number, update: Partial<User>) {
+        const originalUser = await this.findById(id);
+        const newUser = this.userRepository.create({ ...originalUser, ...update });
+
+        return this.userRepository.save(newUser);
+    }
+
+    async remove(id: number) {
+        const user = await this.findById(id);
+        return this.userRepository.remove(user);
+    }
+}
