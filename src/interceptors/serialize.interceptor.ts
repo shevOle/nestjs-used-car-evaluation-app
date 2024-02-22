0173d44046d78c69a2b76f734792eb95fb2f8e5c
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-02-22 21:59:50 +0200

    feat: implement class interface for interceptor typization

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -3,24 +3,28 @@ import {
     UseInterceptors,     
     ExecutionContext, 
     CallHandler,
- } from '@nestjs/common';
- import { Observable, map } from 'rxjs';
- import { plainToInstance } from 'class-transformer';
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+import { plainToInstance } from 'class-transformer';
 
- export function Serialize(dto: any) {
+interface ClassConstructor {
+    new (...args: any[]): {}
+}
+
+export function Serialize(dto: ClassConstructor) {
     return UseInterceptors(new SerializieInterceptor(dto));
- }
+}
 
- export class SerializieInterceptor implements NestInterceptor {
-    constructor(private dto: any) {}
+class SerializieInterceptor implements NestInterceptor {
+    constructor(private dto: ClassConstructor) {}
 
     intercept(_: ExecutionContext, handler: CallHandler): Observable<any> {
         return handler.handle().pipe(
-            map((data: any) => {
+            map((data: ClassConstructor) => {
                 return plainToInstance(this.dto, data, {
                     excludeExtraneousValues: true,
                 });
             })
         )
     }
- }
+}
