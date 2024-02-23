afc4fcb8bc85e82c95e8875af518c55c9800ccba
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-02-23 20:40:52 +0200

    feat: add cookie-sessions

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,15 @@ import {  ValidationPipe } from '@nestjs/common';
 import { config } from 'dotenv';
 import { AppModule } from './app.module';
 
+const cookieSession = require('cookie-session');
+
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({
+    keys: process.env.COOKIE_KEYS.split(','),
+  }))
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true }),
   );
