5a8064f451cb54c36fa873fed51282dbee7bbcdf
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-02-26 22:50:06 +0200

    refactor: update app configuration for e2e tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe, MiddlewareConsumer } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { Report } from './reports/report.entity';
 import { User } from './users/user.entity';
 import { AuthModule } from './auth/auth.module';
+
+const cookieSession = require('cookie-session');
+
+config();
 
 @Module({
   imports: [
@@ -18,5 +24,19 @@ import { AuthModule } from './auth/auth.module';
     ReportsModule,
     AuthModule,
   ],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      })
+    }
+  ]
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cookieSession({
+      keys: process.env.COOKIE_KEYS.split(','),
+    }))
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import {  ValidationPipe } from '@nestjs/common';
-import { config } from 'dotenv';
 import { AppModule } from './app.module';
-
-const cookieSession = require('cookie-session');
-
-config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({
-    keys: process.env.COOKIE_KEYS.split(','),
-  }))
-  app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true }),
-  );
   await app.listen(3000);
 }
 bootstrap();
