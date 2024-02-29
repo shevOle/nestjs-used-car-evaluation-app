25bfc1f50e42c1c802fafc1c541561658a368df7
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-02-29 22:50:28 +0200

    feat: add checkReport endpoint  logic

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Post, Body, UseGuards, Res } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Patch,
+  Param,
+  Body,
+  UseGuards,
+  Res,
+} from '@nestjs/common';
 import { Response as IResponse } from 'express';
 import { ReportsService } from './reports.service';
-import { CreateReportDto } from './dtos/create-report.dto';
+import { CreateReportRequestDto } from './dtos/create-report.request.dto';
+import { CheckReportRquestDto } from './dtos/check-report.request.dto';
 import { AuthGuard } from '../guards/auth.guard';
 import { CurrentUser } from '../decorators/current-user.decorator';
 import { User } from '../entities/user.entity';
@@ -14,10 +23,21 @@ export class ReportsController {
   @Post()
   async createReport(
     @CurrentUser() currentUser: User,
-    @Body() body: CreateReportDto,
+    @Body() body: CreateReportRequestDto,
     @Res() res: IResponse,
   ) {
     await this.reportService.create(currentUser, body);
     return res.sendStatus(201);
+  }
+
+  @Patch('/:id')
+  async checkReport(
+    @CurrentUser() user: User,
+    @Param('id') id: number,
+    @Body() body: CheckReportRquestDto,
+    @Res() res: IResponse,
+  ) {
+    await this.reportService.checkReport(id, user, body.approved);
+    return res.sendStatus(200);
   }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { CreateReportDto } from './dtos/create-report.dto';
+import { CreateReportRequestDto } from './dtos/create-report.request.dto';
 import { Report } from '../entities/report.entity';
 import { User } from '../entities/user.entity';
 
@@ -11,13 +11,22 @@ export class ReportsService {
     @InjectRepository(Report) private reportRepository: Repository<Report>,
   ) {}
 
-  create(currentUser: User, reportData: CreateReportDto) {
+  create(currentUser: User, reportData: CreateReportRequestDto) {
     const currentYear = new Date().getFullYear();
     if (reportData.year > currentYear)
       throw new BadRequestException('You can not sell a car from the future!');
 
     const report = this.reportRepository.create(reportData);
     report.user = currentUser;
+    return this.reportRepository.save(report);
+  }
+
+  async checkReport(id: number, currentser: User, appproved: boolean) {
+    const report = await this.reportRepository.findOne({ where: { id } });
+    if (!report) throw new BadRequestException('Report not found');
+
+    report.updatedByUserId = currentser.id;
+    report.status = appproved ? 'approved' : 'rejected';
     return this.reportRepository.save(report);
   }
 }
