94683a6e3c48e45639763b94e6ff034dbeddb5ff
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-03-02 21:56:36 +0200

    feat: implement adminAuth guard

---
+++ src/guards/admin-auth.guard.ts
@@ -0,0 +1,9 @@
+import { ExecutionContext, CanActivate } from '@nestjs/common';
+
+export class AdminAuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+
+    return request?.currentUser?.admin;
+  }
+}
