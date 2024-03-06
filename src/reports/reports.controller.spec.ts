2a0d679224116d6db2faa252e75e3ac0ef75ed2c
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-03-06 22:52:53 +0200

    feat: add tests for reports controller

--- src/reports/reports.controller.spec.ts
+++ src/reports/reports.controller.spec.ts
@@ -1,17 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Response } from 'express';
 import { ReportsController } from './reports.controller';
 import { ReportsService } from './reports.service';
+import { User } from 'src/db/entities/user.entity';
 
 describe('ReportsController', () => {
   let controller: ReportsController;
+  let fakeReportService: Partial<ReportsService>;
+  const fakeResponseObj: any = {
+    sendStatus: jest.fn(),
+  };
+
+  const defaultUser: User = {
+    id: 1,
+    email: 'test@email.com',
+    password: 'pass',
+    isAdmin: false,
+    reports: [],
+  };
+
+  const defaultReport = {
+    make: 'toyota',
+    model: 'corolla',
+    year: 1980,
+    lat: 1,
+    lng: 2,
+    mileage: 10000,
+    price: 1000,
+  };
 
   beforeEach(async () => {
+    fakeReportService = {
+      getEstimate: jest.fn(),
+      createMany: jest.fn(),
+      findByUserId: jest.fn(),
+      findById: jest.fn(),
+      create: jest.fn(),
+      checkReport: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ReportsController],
       providers: [
         {
           provide: ReportsService,
-          useValue: {},
+          useValue: fakeReportService,
         },
       ],
     }).compile();
@@ -21,5 +54,76 @@ describe('ReportsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('get price estimate', () => {
+    it('passes query down, OK', async () => {
+      await controller.priceEstimate(defaultReport);
+
+      expect(fakeReportService.getEstimate).toHaveBeenCalledWith(defaultReport);
+    });
+  });
+
+  describe('generate reports', () => {
+    it('passes request down, OK', async () => {
+      const reports = Array(3).fill(defaultReport);
+      await controller.createMany(defaultUser, reports, fakeResponseObj);
+
+      expect(fakeReportService.createMany).toHaveBeenCalledWith(
+        defaultUser,
+        reports,
+      );
+      expect(fakeResponseObj.sendStatus).toHaveBeenCalledWith(201);
+    });
+  });
+
+  describe('get own report', () => {
+    it('passes request down, OK', async () => {
+      await controller.getMyReports(defaultUser);
+
+      expect(fakeReportService.findByUserId).toHaveBeenCalledWith(
+        defaultUser.id,
+      );
+    });
+  });
+
+  describe('get report by id', () => {
+    it('passes request down, OK', async () => {
+      const id = 3;
+      await controller.getOne(id);
+
+      expect(fakeReportService.findById).toHaveBeenCalledWith(id);
+    });
+  });
+
+  describe('creates one report', () => {
+    it('passes request down, OK', async () => {
+      await controller.createReport(
+        defaultUser,
+        defaultReport,
+        fakeResponseObj,
+      );
+
+      expect(fakeReportService.create).toHaveBeenCalledWith(
+        defaultUser,
+        defaultReport,
+      );
+      expect(fakeResponseObj.sendStatus).toHaveBeenCalledWith(201);
+    });
+  });
+
+  describe('marks report checkked', () => {
+    it('passes request down, OK', async () => {
+      const body = { approved: false };
+      const id = 4;
+      await controller.checkReport(defaultUser, id, body, fakeResponseObj);
+
+      expect(fakeReportService.checkReport).toHaveBeenCalledWith(
+        id,
+        defaultUser,
+        body.approved,
+      );
+      expect(fakeResponseObj.sendStatus).toHaveBeenCalledWith(200);
+    });
   });
 });
