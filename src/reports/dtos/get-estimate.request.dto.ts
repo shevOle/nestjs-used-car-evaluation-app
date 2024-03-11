891059058d353ec807a0f4952c074e5eb852d20d
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-03-11 22:04:31 +0200

    refactor: update DTO for report estimate request

--- src/reports/dtos/get-estimate.request.dto.ts
+++ src/reports/dtos/get-estimate.request.dto.ts
@@ -5,33 +5,42 @@ import {
   IsLongitude,
   Min,
   Max,
+  IsDefined,
 } from 'class-validator';
 import { Transform } from 'class-transformer';
 
 export class GetEstimateRequestDto {
-  @IsString()
+  @IsString({ message: 'Maker must be a string' })
+  @IsDefined({ message: 'Maker is obligatory' })
   make: string;
 
-  @IsString()
+  @IsString({ message: 'Model must be a string' })
+  @IsDefined({ message: 'Model is obligatory' })
   model: string;
 
   @Transform(({ value }) => parseInt(value))
-  @IsNumber()
-  @Min(0)
-  @Max(1000000)
+  @IsDefined({ message: 'Mileage is obligatory' })
+  @IsNumber({ allowNaN: false })
+  @Min(0, { message: 'Mileage should be a positive number up to 1000000' })
+  @Max(1000000, {
+    message: 'Mileage should be a positive number up to 1000000',
+  })
   mileage: number;
 
   @Transform(({ value }) => parseInt(value))
-  @IsNumber()
-  @Min(1900)
-  @Max(2100)
+  @IsDefined({ message: 'Year is obligatory' })
+  @IsNumber({ allowNaN: false })
+  @Min(1990, { message: 'Year value must be from 1990 to 2030' })
+  @Max(2030, { message: 'Year value must be from 1990 to 2030' })
   year: number;
 
   @Transform(({ value }) => parseFloat(value))
-  @IsLongitude()
+  @IsDefined({ message: 'Longitude is obligatory' })
+  @IsLongitude({ message: 'Longitude must be a number between -180 and 180' })
   lng: number;
 
   @Transform(({ value }) => parseFloat(value))
-  @IsLatitude()
+  @IsDefined({ message: 'Latitude is obligatory' })
+  @IsLatitude({ message: 'Latitude must be a number between -90 and 90' })
   lat: number;
 }
