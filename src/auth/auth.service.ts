a1f44e296e9867f78572b919429fc1c5002bc5f2
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-05-17 18:58:25 +0300

    refactor: return signed token at signup and login requests

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
+
 import { CreateUserRequestDto } from './dtos/create-user.request.dto';
 import { UsersService } from '../users/users.service';
 import { UtilsService } from '../utils/utils.service';
+import { User } from 'src/db/entities/user.entity';
 
 @Injectable()
 export class AuthService {
@@ -10,17 +12,27 @@ export class AuthService {
     private utilsService: UtilsService,
   ) {}
 
-  async signup({ email, password, profilePicture }: CreateUserRequestDto) {
+  async signup({
+    email,
+    password,
+    profilePicture,
+  }: CreateUserRequestDto): Promise<string> {
     const existingUser = await this.userService.findByEmail(email);
     if (existingUser) {
       throw new BadRequestException('This email is already in use');
     }
 
     const hashedPassword = await this.utilsService.hashPassword(password);
-    return this.userService.createUser(email, hashedPassword, profilePicture);
+    const user = await this.userService.createUser(
+      email,
+      hashedPassword,
+      profilePicture,
+    );
+
+    return this.utilsService.prepareToken(user);
   }
 
-  async login(email: string, password: string) {
+  async login(email: string, password: string): Promise<string> {
     const user = await this.userService.findByEmail(email);
 
     if (!user)
@@ -31,6 +43,6 @@ export class AuthService {
     if (!isPasswordCorrect)
       throw new BadRequestException('Username or password is incorrect');
 
-    return user;
+    return this.utilsService.prepareToken(user);
   }
 }
